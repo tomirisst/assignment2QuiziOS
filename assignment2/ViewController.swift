952b23d6e0252c5174ab93b2a69d30f4b1b37d34
tomirissayat <tomiris.tl@gmail.com>
2021-01-31 14:03:45 +0600

    Initial Commit

---
+++ assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment2
+//
+//  Created by Tomiris Sayat on 31.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
